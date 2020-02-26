6932f6a9d9fb866d6d3b8ca2e34fb47b0efd3b84
mariancristu <mariancristu9@gmail.com>
2020-02-26 10:46:25 -0600

    some changes

--- lib/input.rb
+++ lib/input.rb
@@ -9,20 +9,39 @@ class Input
         9.times do |x| 
           if x % 2 == 0
             puts question(players[0])
+            p br_arr
             value = gets.chomp
-            br_arr[value.to_i-1] = "X"
-            Board.render(br_arr)
+            until is_free?(br_arr,value)
+              puts "Try again"
+              value = gets.chomp
+            end
+            mod_arr(br_arr,value,"X")
           else
+            p br_arr
             puts question(players[1])
             value = gets.chomp
-            p value
-            br_arr[value.to_i-1] = "O"
-            Board.render(br_arr)
+            until is_free?(br_arr,value)
+              puts "Try again"
+              value = gets.chomp
+            end
+            mod_arr(br_arr,value,"O")
           end
         end
     end
 
+
+    protected
+
     def question (player)
         "#{ player } select your movement"    
+    end
+
+    def mod_arr(arr, position, val)
+      arr[position.to_i-1] = val
+      Board.render(arr)
+    end
+
+    def is_free? (arr,position)
+      (arr[position.to_i-1] != "X" && arr[position.to_i-1] != "O")
     end
 end
