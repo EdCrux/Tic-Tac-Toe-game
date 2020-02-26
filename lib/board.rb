8dfadf96780d4cde820fab562cc88be292fa7ef1
mariancristu <mariancristu9@gmail.com>
2020-02-26 08:58:50 -0600

    :art: Improving code

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,8 @@ module Board
         count = 0
         
         3.times do |x|
-            puts " #{m[0+count].send(color(m[0+count])) } | #{m[1+count].send(color(m[1+count]))} | #{m[2+count].send(color(m[2+count]))}".center(width)
+            a = " #{m[0+count].send(color(m[0+count])) } | #{m[1+count].send(color(m[1+count]))} | #{m[2+count].send(color(m[2+count]))}"
+            puts a.center(width+40)
             puts "-----------".center(width) if count < 6
             count += 3
         end
