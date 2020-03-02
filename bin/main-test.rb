9971c305073c0f0e7d759229211b9b98b5975bc5
mariancristu <mariancristu9@gmail.com>
2020-03-02 10:39:01 -0600

    :sparkles: Add render board method

--- bin/main-test.rb
+++ bin/main-test.rb
@@ -6,6 +6,7 @@ require 'rubygems'
 
 @width = TTY::Screen.width
 @players = []
+@board = %w[1 X 3 4 5 6 7 8 9]
 
 def get_logo
     logo = []
@@ -17,11 +18,25 @@ def get_logo
     logo
 end
 
-def render(logo)
+def render(logo, arr = nil , color = :blue, on_color = :on_red)
     print_logo(logo)
     puts ""
     puts line(@width, "-")
     puts ""
+    print_board(arr, color, on_color) if arr != nil
+end
+
+def print_board(arr, color, on_color)
+    puts line(13,"-").send(color)
+    [0,3,6].each do |x|
+        3.times do |y|
+            print "|".send(color)
+            print arr[y+x].between?("1","9") ? " #{arr[y+x]} ".send(on_color) : " #{arr[y+x]} "
+        end
+    print "|".send(color)
+    puts ""
+    puts line(13,"-").send(color)
+    end
 end
 
 def print_logo(logo)
@@ -39,22 +54,17 @@ def line(width, sign)
     line
 end
 
-
-
 render(get_logo)
-
-def valid_name(player)
-    until player.length.positive?
-        puts "Give a valid name"
-        player = gets.chomp
-    end
-    @players << player
-end
 
 2.times do |x|
   puts "Please enter the name of the #{x == 0 ? 'first' : 'second'}  player"
   player = gets.chomp
-  valid_name(player)
+  until player.length.positive?
+    puts "Enter a valid name:".red
+    player = gets.chomp
+  end
+  @players << player
 end
 
 
+render(get_logo, @board, :green, :on_blue)
